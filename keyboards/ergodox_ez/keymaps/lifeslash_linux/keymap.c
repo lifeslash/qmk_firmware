d61501dcc56eafd6ca0f164fc387870c69dc7549
again <lifeslash.mail@gmail.com>
2024-01-04 03:00:12 +0000

    CAPSLOCKキーの代わりに、WORDCAPSEを使うように修正

--- keyboards/ergodox_ez/keymaps/lifeslash_linux/keymap.c
+++ keyboards/ergodox_ez/keymaps/lifeslash_linux/keymap.c
@@ -28,16 +28,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | Tab    | Q/L1 |   W  |   E  |   R  |   T  |  '   |           |   \  |   Y  |   U  |   I  |   O  | P/L1 |  DEL   |
  * |--------+------+------+------+------+------|  Cmd |           |  Cmd |------+------+------+------+------+--------|
- * |CAPS/Alt|   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |; / L2|APP/Alt |
+ * |WordCAPS|   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |; / L2|APP/Alt |
  * |--------+------+------+------+------+------|   =  |           |   -  |------+------+------+------+------+--------|
  * | LShift(|Z/Ctrl|   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |//Ctrl| RShift)|
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   | Grv  |  '"  | Esc  | Left | Down |                                       |  Up  | Right|   [  |   ]  | ~L1  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
- *                                        |      |DM_STP|       |QK_LCK|        |
+ *                                        |      |DM_RC1|       |      |        |
  *                                 ,------|------|------|       |------+--------+------.
- *                                 |      |      |DM_RC1|       |DM_RC2|        |      |
+ *                                 |      |      |DM_STP|       |DM_RC2|        |      |
  *                                 | Space|Backsp|------|       |------|  Han   |Enter |
  *                                 |      |ace   |DM_PL1|       |DM_PL2|        |      |
  *                                 `--------------------'       `----------------------'
@@ -46,11 +46,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // left hand
   LCTL(LALT(KC_LEFT)),KC_1,     KC_2,       KC_3,    KC_4,    KC_5,    KC_6,
   KC_TAB,          LT(SYMB,KC_Q),KC_W,      KC_E,    KC_R,    KC_T,    GUI_T(KC_QUOT),
-  LALT_T(KC_CAPS), KC_A,        KC_S,       KC_D,    KC_F,    KC_G,
+  CW_TOGG,         KC_A,        KC_S,       KC_D,    KC_F,    KC_G,
   SC_LSPO,         CTL_T(KC_Z), KC_X,       KC_C,    KC_V,    KC_B,    KC_EQL,
   KC_GRV,          KC_QUOT,     KC_ESC,     KC_LEFT, KC_DOWN,
-                                                                 KC_NO,   DM_RSTP,
-                                                                          DM_REC1,
+                                                                 KC_NO,   DM_REC1,
+                                                                          DM_RSTP,
                                                          KC_SPC, KC_BSPC, DM_PLY1,
   // right hand
   KC_7,         KC_8,    KC_9,    KC_0,    ALT_TAB,           LCTL(LALT(KC_T)),   LCTL(LALT(KC_RGHT)),
@@ -58,8 +58,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_H,         KC_J,    KC_K,    KC_L,    LT(MDIA, KC_SCLN), LALT_T(KC_APP),
   KC_MINS,      KC_N,    KC_M,    KC_COMM, KC_DOT,            CTL_T(KC_SLSH),     SC_RSPC,
   KC_UP,        KC_RGHT, KC_LBRC, KC_RBRC, TT(SYMB),
-  QK_LOCK, KC_NO,
-  DM_REC2,
+  DM_REC2,   KC_NO,
+  DM_RSTP,
   DM_PLY2, LCTL(KC_SPC), KC_ENT
 ),
 /* Keymap 1: Symbol Layer
@@ -69,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |---------+------+------+------+------+------+------|           |------+------+------+------+------+------+--------|
  * |RESET    |      |   @  |  $   |  ^   |  *   |   [  |           |  ]   |   7  |   8  |   9  |   *  |   /  |   INS  |
  * |---------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |         |   !  |   #  |  %   |  &   | ESC  |------|           |------|   4  |   5  |   6  |   +  |   -  |        |
+ * |CAPS     |   !  |   #  |  %   |  &   | ESC  |------|           |------|   4  |   5  |   6  |   +  |   -  |        |
  * |---------+------+------+------+------+------|   <  |           |  >   |------+------+------+------+------+--------|
  * |         |      |      |      | jis\ | jis_ |      |           |      |   1  |   2  |   3  |   0  |   =  |        |
  * `---------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
@@ -87,7 +87,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // left hand
   VRSN,    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,
   QK_BOOT, KC_NO,   KC_AT,   KC_DLR,  KC_CIRC, KC_ASTR, KC_LBRC,
-  KC_NO,   KC_EXLM, KC_HASH, KC_PERC, KC_AMPR, KC_ESC,
+  KC_CAPS, KC_EXLM, KC_HASH, KC_PERC, KC_AMPR, KC_ESC,
   KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_INT3, KC_INT1, LSFT(KC_COMM),
   EE_CLR,  KC_NO,   KC_NO,   KC_HOME, KC_PGDN,
                                                RGB_MOD, KC_NO,
@@ -151,7 +151,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
       case VRSN:
         // SEND_STRING (QMK_KEYBOARD "/" QMK_KEYMAP " @ " QMK_VERSION);
-        SEND_STRING ("ergodox_ez/lifeslash_linux @ v1.1.0.3");
+        SEND_STRING ("ergodox_ez/lifeslash_linux @ v1.1.0.5");
         return false;
       case ALT_TAB:
         if (!is_alt_tab_active) {
@@ -214,7 +214,7 @@ layer_state_t layer_state_set_user(layer_state_t state) {
           rgblight_setrgb(RGBLIGHT_COLOR_LAYER_2);
         #endif
         break;
-      case 3:
+      /*case 3:
         ergodox_right_led_3_on();
         #ifdef RGBLIGHT_COLOR_LAYER_3
           rgblight_setrgb(RGBLIGHT_COLOR_LAYER_3);
--- keyboards/ergodox_ez/keymaps/lifeslash_win/rules.mk
+++ keyboards/ergodox_ez/keymaps/lifeslash_win/rules.mk
@@ -1,7 +1,8 @@
 RGBLIGHT_ENABLE = no
 RGB_MATRIX_ENABLE = yes # enable later
 DYNAMIC_MACRO_ENABLE = yes
-KEY_LOCK_ENABLE = yes
+CAPS_WORD_ENABLE = yes
+#TAP_DANCE_ENABLE = yes
 
 #RGB_MATRIX_CUSTOM_USER = yes # custom rgb lighting
 #SRC += ../default/keymap.c
