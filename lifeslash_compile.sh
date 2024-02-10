79ec610d2713dc45c154eb865b1624bb5f4e0fab
again <lifeslash.mail@gmail.com>
2024-02-10 07:48:31 +0000

    remove unused files.

--- lifeslash_compile.sh
+++ lifeslash_compile.sh
@@ -1,6 +1,6 @@
 linuxHex="ergodox_ez_base_lifeslash_linux.hex"
 winHex="ergodox_ez_base_lifeslash_win.hex"
-macHex="ergodox_ez_base_lifeslash_mac_ja.hex"
+#macHex="ergodox_ez_base_lifeslash_mac_ja.hex"
 
 if [ -e $linuxHex ]; then
     rm $linuxHex
@@ -8,9 +8,9 @@ fi
 if [ -e $winHex ]; then
     rm $winHex
 fi
-if [ -e $macHex ]; then
-    rm $macHex
-fi
+#if [ -e $macHex ]; then
+#    rm $macHex
+#fi
 qmk compile -kb ergodox_ez -km lifeslash_linux
 qmk compile -kb ergodox_ez -km lifeslash_win
 #qmk compile -kb ergodox_ez -km lifeslash_mac_ja
