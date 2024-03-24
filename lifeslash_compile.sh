9b27acadaf62c51385a5fafac44aa7db2c4f929b
again <lifeslash.mail@gmail.com>
2024-03-24 04:37:02 +0000

    update

--- lifeslash_compile.sh
+++ lifeslash_compile.sh
@@ -1,6 +1,5 @@
 linuxHex="ergodox_ez_base_lifeslash_linux.hex"
 winHex="ergodox_ez_base_lifeslash_win.hex"
-#macHex="ergodox_ez_base_lifeslash_mac_ja.hex"
 
 if [ -e $linuxHex ]; then
     rm $linuxHex
@@ -8,9 +7,6 @@ fi
 if [ -e $winHex ]; then
     rm $winHex
 fi
-#if [ -e $macHex ]; then
-#    rm $macHex
-#fi
+
 qmk compile -kb ergodox_ez -km lifeslash_linux
 qmk compile -kb ergodox_ez -km lifeslash_win
-#qmk compile -kb ergodox_ez -km lifeslash_mac_ja
