584cd64fd3b593a0fde6d7d1e0f77bad26b54677
again <lifeslash.mail@gmail.com>
2024-07-13 15:02:34 +0900

    eucalyn配列を追加

--- lifeslash_compile.sh
+++ lifeslash_compile.sh
@@ -1,5 +1,6 @@
 linuxHex="ergodox_ez_base_lifeslash_linux.hex"
 winHex="ergodox_ez_base_lifeslash_win.hex"
+eucalynHex="ergodox_ez_base_lifeslash_eucalyn.hex"
 
 if [ -e $linuxHex ]; then
     rm $linuxHex
@@ -7,6 +8,10 @@ fi
 if [ -e $winHex ]; then
     rm $winHex
 fi
+if [ -e $eucalynHex ]; then
+    rm $eucalynHex
+fi
 
 qmk compile -kb ergodox_ez -km lifeslash_linux
 qmk compile -kb ergodox_ez -km lifeslash_win
+qmk compile -kb ergodox_ez -km lifeslash_eucalyn
